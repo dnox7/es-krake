494916c165159e1943e40de0edc9dab8cd02d46d
Dang Hai <pecheule34713@gmail.com>
2025-02-11 11:50:02 +0700

    lmao rancher:(

--- internal/infrastructure/repository/product/product_option_repo.go
+++ internal/infrastructure/repository/product/product_option_repo.go
@@ -27,10 +27,8 @@ func (r *productOptionRepository) TakeByConditions(ctx context.Context, conditio
 	var productOption entity.ProductOption
 
 	sql, args, err := r.pg.Builder.
-		Select("po.id", "po.product_id", "po.name", "po.description", "po.created_at", "po.updated_at", ).
-		From(domainRepo.ProductOptionTableName + "AS po").
-        InnerJoin(domainRepo.ProductAttributeValueTableName + "AS pav ON pav.product_option_id = po.id").
-        InnerJoin(domainRepo.AttributeTableName + "AS pa ON pav.attribute_id = pa.id")
+		Select("id", "product_id", "name", "description", "created_at", "updated_at").
+		From(domainRepo.ProductOptionTableName).
 		Where(sq.Eq(conditions)).
 		ToSql()
 
@@ -45,5 +43,31 @@ func (r *productOptionRepository) TakeByConditions(ctx context.Context, conditio
 }
 
 func (r *productOptionRepository) FindByConditions(ctx context.Context, conditions map[string]interface{}) ([]entity.ProductOption, error) {
-    var 
+	var options map[int]*entity.ProductOption
+
+	sql, args, err := r.pg.Builder.
+		Select(
+			"po.id", "po.product_id", "po.name", "po.description", "po.created_at", "po.updated_at",
+			"pav.id", "pav.value",
+			"pa.id", "pav.name",
+		).
+		From(domainRepo.ProductOptionTableName + " AS po").
+		InnerJoin(domainRepo.ProductAttributeValueTableName + " AS pav ON pav.product_option_id = po.id").
+		InnerJoin(domainRepo.AttributeTableName + " AS pa ON pa.id = pav.attribute_id").
+		Where(sq.Eq(conditions)).
+		ToSql()
+
+	if err != nil {
+		r.logger.Error(utils.ErrQueryBuilderFailedMsg)
+		return nil, err
+	}
+
+	rows, err := r.pg.DB.QueryxContext(ctx, sql, args...)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+
+	}
 }
