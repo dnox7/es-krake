729ae42544297f0d8ec0e6e7cae09024470b56ac
Dang Hai <pecheule34713@gmail.com>
2025-04-24 01:34:59 +0700

    update mongo scope

--- internal/domain/product/repository/product_meta_repo.go
+++ internal/domain/product/repository/product_meta_repo.go
@@ -1,3 +1,15 @@
 package repository
 
+import (
+	"context"
+	"pech/es-krake/internal/domain/product/entity"
+	"pech/es-krake/internal/domain/shared/scope"
+)
+
 const ProductMetaTableName = "productmetas"
+
+type ProductMetaRespository interface {
+	TakeByConditions(ctx context.Context, filter interface{}, scopes ...scope.Base) (entity.ProductMeta, error)
+
+	FindByConditions(ctx context.Context, filter interface{}, scopes ...scope.Base) ([]entity.ProductMeta, error)
+}
--- internal/infrastructure/mongodb/scope.go
+++ internal/infrastructure/mongodb/scope.go
@@ -1,10 +1,31 @@
 package mdb
 
 import (
-	"go.mongodb.org/mongo-driver/v2/bson"
+	"pech/es-krake/internal/domain/shared/scope"
+
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
-func Sort(d bson.D) {
-	opts := options.Find().SetSort(d)
+type findScope struct {
+	builder *options.FindOptionsBuilder
+}
+
+func FindScope(builder *options.FindOptionsBuilder) scope.Base {
+	return &findScope{builder}
+}
+
+func (fs *findScope) GetScope() interface{} {
+	return fs.builder
+}
+
+type findOneScope struct {
+	builder *options.FindOneOptionsBuilder
+}
+
+func FindOneScope(builder *options.FindOneOptionsBuilder) scope.Base {
+	return &findOneScope{builder}
+}
+
+func (fos *findOneScope) GetScope() interface{} {
+	return fos.builder
 }
